19ae71c4a56ee9e5ca6941871025b45133ca33cb
Oliver Smith <osmith8@sheffield.ac.uk>
2026-03-17 15:52:41 +0000

    Add mock tests for the random function

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -3,6 +3,21 @@ from lecture import randomised_function
 
 class MyTestCase(unittest.TestCase):
 
-    def test_randomised_function(self):
-        self.assertEqual('software', randomised_function())  # This will pass or fail randomly
-        # TODO: Can we make this test deterministic? (HINT: Mock testing)
+    # def test_randomised_function(self):
+    #     self.assertEqual('software', randomised_function())  # This will pass or fail randomly
+    #     # TODO: Can we make this test deterministic? (HINT: Mock testing)
+
+
+
+    # Taken from the solution as this is better than what I did
+    @patch('lecture.is_small')  # Mock the is_small function
+    def test_randomised_function_with_mock1(self, mymock_is_small):
+        mymock_is_small.return_value = True
+        self.assertEqual('software', randomised_function())
+
+    @patch('lecture.is_small')  # Mock the is_small function
+    def test_randomised_function_with_mock2(self, mymock_is_small):
+        mymock_is_small.return_value = False
+        self.assertEqual('engineering', randomised_function())
+
+        
